e10085f930a31434d53044d899d1c88f0f572987
Winston  Chung <18.chung.winston@ctkprep.org>
2019-12-07 22:02:26 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,9 +4,9 @@ class Show < ActiveRecord::Base
   belongs_to :network
   
   #def actors_list
-  #  self.actors.map do |actor|
+    self.actors.map do |actor|
       
-   #   "#{actor.full_name}"
-  #  end
- # end
+      "#{actor.full_name}"
+    end
+  end
 end
